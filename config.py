597a59265a49b05702e1b63f3a6114cd720b8204
teachinginbinary <teachinginbinary@gmail.com>
2024-06-14 14:07:23 -0400

    new db url

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 from secrets import token_hex
 
 SECRET_KEY = token_hex(16)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+SQLALCHEMY_DATABASE_URI = 'postgres://blog_user_db_user:aYcsq2E5YPHCn7JIK7duSpD0QSAXNrX5@dpg-cpm8c5qju9rs738mhnhg-a.oregon-postgres.render.com/blog_user_db'
